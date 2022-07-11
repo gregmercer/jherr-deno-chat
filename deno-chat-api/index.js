ce321230455e415e13354df655ae62eed5e2086a
Greg Mercer <gmercer@stanford.edu>
2022-07-10 17:05:25 -0700

    Adding router.

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,9 +1,15 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Chat server!";
+  });
 
 const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello World from Deno!";
-});
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
