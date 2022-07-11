84ae0296a052b97e42568da78ae13d47cd2590ae
Greg Mercer <gmercer@stanford.edu>
2022-07-11 05:29:35 -0700

    Removing unneeded import.

--- deno-chat-client/routes/index.tsx
+++ deno-chat-client/routes/index.tsx
@@ -1,6 +1,5 @@
 /** @jsx h */
 import { h, IS_BROWSER } from "preact";
-import { useEffect, useState, useCallback } from "preact/hooks";
 import Messages from "../islands/Messages.tsx";
 
 export default function Home() {
